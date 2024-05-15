c36c6af9476bab69120bf9428ff49b8c0f93ab69
Ydra17 <ardyantobaso@gmail.com>
2024-05-15 13:33:52 +0800

    user management

---
+++ resources/views/pages/users/index.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.app')
+
+@section('title', 'Table')
+
+@push('style')
+    <!-- CSS Libraries -->
+@endpush
+
+@section('main')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Users &nbsp;&nbsp;</h1>
+                <div class="section-haeder-button">
+                    <a href="{{route('users.create')}}" class="btn btn-primary">Add New User</a>
+                </div>
+            </div>
+
+            <div class="section-body">
+                <div class="row">
+                    <div class="col-12">
+                        @include('layouts.alert')
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-12 col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>User Table</h4>
+                                <div class="card-header-form">
+                                    <form method="GET" action="{{route('users.index')}}">
+                                        <div class="input-group">
+                                            <input type="text"
+                                                class="form-control"
+                                                placeholder="Search" name="name">
+                                            <div class="input-group-btn">
+                                                <button class="btn btn-primary"><i class="fas fa-search"></i></button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table-striped table-md table">
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Phone</th>
+                                            <th>Created At</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        @foreach ($users as $user)
+                                        <tr>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                            <td>{{ $user->phone }}</td>
+                                            <td>{{ $user->created_at }}</td>
+                                            <td>
+                                                <div class="d-flex justify-content-center">
+                                                    <a href="{{ route('users.edit', $user->id) }}" class="btn btn-sm btn-primary"><i class="fas fa-edit"></i> Edit</a>
+                                                    <form action="{{ route('users.destroy', $user->id) }}"
+                                                        method="POST" class="ml-2">
+                                                        <input type="hidden" name="_method" value="DELETE" />
+                                                        <input type="hidden" name="_token"
+                                                            value="{{ csrf_token() }}" />
+                                                        <button class="btn btn-sm btn-danger btn-icon confirm-delete">
+                                                            <i class="fas fa-times"></i> Delete
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="card-footer text-right">
+                                {{$users->withQueryString()->links()}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+    <script src="{{ asset('library/jquery-ui-dist/jquery-ui.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('js/page/components-table.js') }}"></script>
+@endpush
