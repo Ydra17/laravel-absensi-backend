37b89181c65ef07ea16945e64d462dc11d7865ea
Ydra17 <ardyantobaso@gmail.com>
2024-05-15 20:45:52 +0800

    add login and logout api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//login
+Route::post('/login', [AuthController::class, 'login']);
+
+//logout
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
