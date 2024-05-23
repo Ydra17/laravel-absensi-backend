2ee219795c75f248fecb6f7ab1a933a6c7f5a408
Ydra17 <ardyantobaso@gmail.com>
2024-05-23 17:20:28 +0800

    company dashboard and api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Company;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -20,6 +21,17 @@ class DatabaseSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@mail.com',
             'password' => Hash::make('12345678'),
+        ]);
+
+        Company::create([
+            'name' => 'PT. Mencari Cinta Sejati',
+            'email' => 'mencaricintasejati@mail.com',
+            'address' => 'Jalan setia hati menunggu nomor 12',
+            'latitude' => '-7.747033',
+            'longitude' => '110.355398',
+            'radius_km' => '0.5',
+            'time_in' => '08:00',
+            'time_out' => '17:00',
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CompanyController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,5 @@ Route::post('/login', [AuthController::class, 'login']);
 //logout
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
+//get company
+Route::get('/company', [CompanyController::class, 'index'])->middleware('auth:sanctum');
